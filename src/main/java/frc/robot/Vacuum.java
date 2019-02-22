1f321d509a292420b044c4c810d6f5b581c5732a
Matt <matt@Matts-Air.frontier.com>
2019-02-21 19:27:42 -0500

    Added config class and vacuum class

---
+++ src/main/java/frc/robot/Vacuum.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+class Vacuum {
+    private TalonSRX vacuum;
+
+    public Vacuum(TalonSRX vacuum_motor) {
+
+        this.vacuum = vacuum_motor;
+        
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/frc/robot/config.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+class config {
+
+public static int can_drive_left = 1;
+public static int can_drive_right = 2;
+public static int can_elevator = 5;
+public static int can_intake_arm = 6;
+public static int can_intake_roller = 7;
+public static int can_vacuum_pump = 8;
+public static int can_PCM_0 = 9;
+
+
+
+}
